6f3d6e8f4ad8f090767e15fe050d83cfbfdaede7
Vickyvky13 <131648662+Vickyvky13@users.noreply.github.com>
2023-07-28 11:05:41 +0530

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -205,7 +205,8 @@ async def bard_bot(bot, message):
         else:
             a = message.text.split(' ', 1)[1]
             response=bard.get_answer(f"{a}")["content"]
-            await message.reply_text(f"{response}\n\n🎉ᴘᴏᴡᴇʀᴇᴅ ʙʏ @{BOT_USERNAME} ", parse_mode=ParseMode.MARKDOWN,reply_markup=InlineKeyboardMarkup(X))     
+            await message.reply_text(f"Asᴋᴇᴅ Bʏ:{message.from_user.first_n
+ame}\n\nQυeѕтιoɴ ιѕ : {a}\n\nS⃟ᴏʟᴏ ᴛʀᴇᴇ ᴀɴꜱᴡᴇʀᴇᴅ:-\n\n{response}\n\n✨ᴘᴏᴡᴇʀᴇᴅ ʙʏ @{BOT_USERNAME}", parse_mode=ParseMode.MARKDOWN,reply_markup=InlineKeyboardMarkup(X))     
     except Exception as e:
         await message.reply_text(f"**ᴇʀʀᴏʀ:  {e} ")
 
