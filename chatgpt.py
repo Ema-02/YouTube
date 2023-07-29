fa425a421a0d25a2a1516526fa071a2441460b83
Vickyvky13 <131648662+Vickyvky13@users.noreply.github.com>
2023-07-29 10:53:32 +0530

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -22,7 +22,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
 
 
-@Client.on_message(~filters.incoming & filters.private, group=-1)
+@Client.on_message(~filters.edited & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
     if not "https://t.me/cczza":  # Not compulsory
         return
