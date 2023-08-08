e4c0f100a5303919666367a410e5d5534f64eb8e
Vickyvky13 <131648662+Vickyvky13@users.noreply.github.com>
2023-08-08 22:15:47 +0530

    Update must_join.py

--- ChatG/must_join.py
+++ ChatG/must_join.py
@@ -10,8 +10,8 @@ async def must_join_channel(bot: Client, msg: Message):
         return
     try:
         try:
-            userid = message.from_user.id 
-            user_name = message.from_user.first_name
+            userid = message.from_user.id 
+            user_name = message.from_user.first_name
             await bot.get_chat_member(MUST_JOIN, msg.from_user.id)
         except UserNotParticipant:
             if MUST_JOIN.isalpha():
