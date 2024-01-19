9b8f770b236c6d8ad6f9d6dffc094c0a879749e9
Ema-02 <152199222+Ema-02@users.noreply.github.com>
2024-01-20 02:31:00 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvali
 
 logging.basicConfig(level=logging.INFO, encoding="utf-8", format="%(asctime)s - %(levelname)s - \033[32m%(pathname)s: \033[31m\033[1m%(message)s \033[0m")
 
-app = bot(
+bot = Client(
     "Session_bot",
     api_id=config.API_ID,
     api_hash=config.API_HASH,
