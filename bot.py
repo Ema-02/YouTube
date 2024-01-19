6c875f9cc76b02ab0c1c28874e9bf610ee06a773
Ema-02 <152199222+Ema-02@users.noreply.github.com>
2024-01-20 02:46:47 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import config
 import logging
 from pyrogram import Client, idle
-from pyromod import listen 
+from pyrogram import listen 
 from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
 
 
