eafaf7b7d4510379a3d607c2e8a62314a9c54af6
Ema-02 <152199222+Ema-02@users.noreply.github.com>
2024-01-20 02:55:34 +0300

    Update must_join.py

--- ChatG/must_join.py
+++ ChatG/must_join.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
 
-@bot.on_message(filters.incoming & filters.private, group=-1)
+@Client.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
     if not "https://t.me/cczza":  # Not compulsory
         return
